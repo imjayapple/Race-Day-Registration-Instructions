1ff4ab9c2e124c17c9d5b70f499ed127dafd0b19
imjayapple <thisisfakeemail@gmai.com>
2023-01-03 22:22:49 -0800

    4 additional control statement for early/late registered runners & adults/younger-than-adults, and an else statement

--- app.js
+++ app.js
@@ -9,7 +9,26 @@ const age = 21;
 
 //Implement a control flow statement, here, determine if the applicant registered early and is an Adult,
 //if yes then add +1000 to their race number
-if (earlyRegistration && age >= 21)
+if (earlyRegistration && age >= 21) {
     raceNumber += 1000;
+}
 
-    
+//Provide race time and race number to specific runners that registered early and are adults.
+if (earlyRegistration && age >= 21) {
+    console.log(`Your race time will start at 9:30AM, and your race number is: ${raceNumber}`);
+}
+
+//Provide race time and race number to specific runners that registered late and are adults.
+else if(!earlyRegistration && age >= 21 {
+    console.log(`Your race time will start at 11:00AM, and your race number is: ${raceNumber}`)
+})
+
+//Provide race time and race number to specific runners that are younger than adults.
+else if(age < 21) {
+    console.log(`Your race time will start at 12:00PM, and your race number is: ${raceNumber}`)
+}
+
+//Catchall for runners that do not submit correct information
+else {
+    console.log("Please approach the registration desk for assistance")
+}
