e8bfa43dade341ba3d1141e077f3add668293c56
imjayapple <thisisfakeemail@gmai.com>
2023-01-03 22:11:19 -0800

    Add registration status, age variable, and control flow statement

--- app.js
+++ app.js
@@ -1,3 +1,15 @@
 //Generate a random race number, a whole number between 0 (inclusive) & 1,000 (exclusive)
 let raceNumber = Math.floor(Math.random() * 1000);
 
+//Boolean value assigned to variable to determine early registraiton or late
+const earlyRegistration = false;
+
+//Capture the age of a runner and set it equal to a number
+const age = 21;
+
+//Implement a control flow statement, here, determine if the applicant registered early and is an Adult,
+//if yes then add +1000 to their race number
+if (earlyRegistration && age >= 21)
+    raceNumber += 1000;
+
+    
